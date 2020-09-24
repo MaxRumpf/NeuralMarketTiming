5c64cd1f932dc3308dee2b49633dd5d80ed616f4
Max <max.rump@me.com>
2020-09-24 17:34:54 +0200

    improvements

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
+import quandl
+
+quandl.ApiConfig.api_key = "ku1U8pjGaWCy9sJMiFWG"
 
 msft = yf.Ticker("^GSPC")
 numYears = 10
@@ -63,6 +66,54 @@ closeData["spotTo50"] = closeData["Close"] - closeData["sma50"]
 closeData["spotTo100"] = closeData["Close"] - closeData["sma100"]
 closeData["spotTo200"] = closeData["Close"] - closeData["sma200"]
 
+def dateToYYYYMMDD(date, *args):
+    day = date.day
+    if len(args) > 0:
+        day = args[0]
+    return str(date.year) + "-" + str(date.month) + "-" + str(day)
+
+#Other data, this time from Quandl
+peRatioData = quandl.get("MULTPL/SP500_PE_RATIO_MONTH")
+#divYieldData = quandl.get("MULTPL/SP500_DIV_YIELD_MONTH")
+vixData = [quandl.get("CHRIS/CBOE_VX1"), quandl.get("CHRIS/CBOE_VX2"), quandl.get("CHRIS/CBOE_VX3")]
+peRatios = []
+vixValues = [[], [], []]
+#divYields = []
+for date, row in closeData.iterrows():
+    #PE Ratio
+    peRatio = peRatioData["Value"][dateToYYYYMMDD(date, 1)]
+    peRatios.append(peRatio)
+    #DIV Yield
+    #divYield = divYieldData["Value"][dateToYYYYMMDD(date, 1)]
+    #divYields.append(divYield)
+    for x in range(0,3):
+        if dateToYYYYMMDD(date) in vixData[x]["Open"]:
+            vixValue = vixData[x]["Settle"][dateToYYYYMMDD(date)]
+        else:
+            vixValue = vixValues[x][len(vixValues[x]) - 1]
+        vixValues[x].append(vixValue)
+
+closeData["peRatio"] = peRatios
+closeData["vix1"] = vixValues[0]
+closeData["vix2"] = vixValues[1]
+closeData["vix3"] = vixValues[2]
+
+closeData["vix1-3"] = closeData["vix1"] - closeData["vix3"]
+closeData["vix2-3"] = closeData["vix2"] - closeData["vix3"]
+closeData["vix1-2"] = closeData["vix1"] - closeData["vix2"]
+#closeData["divYield"] = divYields
+fig, axs = plt.subplots(2)
+axs[0].plot(closeData.loc[:, ["Close", "vix1"]], 'tab:blue')
+axs[0].set_title('SP500')
+axs[1].plot(closeData.loc[:, "peRatio"], 'tab:orange')
+axs[1].set_title('PE Ratio')
+#axs[2].plot(closeData.loc[:, "vix"], 'tab:green')
+#axs[2].set_title('Vol Price')
+#axs[2].plot(closeData.loc[:, "divYield"], 'tab:green')
+#axs[2].set_title('Dividend Yield')
+
+fig.show()
+
 #closeData.plot(linewidth=0.3)
 
 #for x in range(2000, 3500, 250):
@@ -77,19 +128,20 @@ from keras.layers import Dense
 
 from keras.optimizers import SGD
 # split into input (X) and output (y) variables
-X = closeData.loc[:, ["Close", "sma50", "sma100", "sma200", "50-100", "50-200", "100-200", "spotTo50", "spotTo100", "spotTo200"]].values
+# "vix2", "vix3", "vix1-3", "vix2-3", "vix1-2"
+X = closeData.loc[:, ["sma50", "sma100", "sma200", "50-100", "50-200", "100-200", "spotTo50", "spotTo100", "spotTo200", "vix1", "vix2", "vix3", "vix1-3", "vix2-3", "vix1-2", "peRatio"]].values
 #print(X)
 y = closeData.loc[:, ["outcome"]].values
 # define the keras model
 model = Sequential()
-model.add(Dense(20, input_dim=10, activation='relu'))
-#model.add(Dense(20, activation='relu'))
+model.add(Dense(20, input_dim=16, activation='relu'))
+model.add(Dense(20, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 opt = SGD(lr=0.01, momentum=0.9)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-model.fit(X, y, epochs=500, batch_size=10)
+model.fit(X, y, epochs=500, batch_size=5)
 # evaluate the keras model
 _, accuracy = model.evaluate(X, y)
 #print('Accuracy: %.2f' % (accuracy*100))
